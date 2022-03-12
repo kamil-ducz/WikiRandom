580868726d84a2d91522c86858b1ba189564e3c6
Kamil D <kamilducz1989@gmail.com>
2022-03-12 14:59:07 +0100

    removed unneccesary dir

--- WikiRandom.Lib/Helpers/ConstDefinitionsOld.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ServicesOld
-{
-    public class ConstDefinitionsOld
-    {
-        public const string URL = "https://en.wikipedia.org/wiki/Special:Random";
-    }
-}
--- WikiRandom.Lib/Interfaces/IGetWikiRandom.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace ServicesOld
-{
-    public interface IGetWikiRandom //interface supports retreving random article's header from Wikipedia
-    {
-        Task<string> GetWikiRandom();   //one async method to handle the request
-    }
-}
